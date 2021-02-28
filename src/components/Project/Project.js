bc68043d332afa49cc2c507e8e15b2a913ba6767
Raj Kumar Panda <rkp1819@gmail.com>
2021-02-28 20:57:40 +0530

    Intro

---
+++ src/components/Project/Project.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Project.css";
+function Project() {
+  return <div className="project">some shit</div>;
+}
+
+export default Project;
---
+++ src/components/ShowcaseWork/ShowcaseWork.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ShowcaseWork.css";
+import Project from "../Project/Project";
+
+function ShowcaseWork() {
+  return (
+    <div className="showcaseWork">
+      <Project />
+    </div>
+  );
+}
+
+export default ShowcaseWork;
